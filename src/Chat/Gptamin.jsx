fc65ba58f949e1b4ba7c32ef243309656b0f3ed9
Mohit Rajpoot <84062871+rajpootmohit782@users.noreply.github.com>
2023-07-06 19:03:02 +0000

    complete

---
+++ src/Chat/Gptamin.jsx
@@ -0,0 +1,16 @@
+function Gptamin() {
+    return (
+      
+        <div className='w-[80%]'>
+  <div className="h-[80%] flex flex-col justify-center items-center" style={{ color: 'white' }}>
+  <div>Chatgpt</div>
+</div>
+
+</div>
+
+      
+    );
+  }
+  
+  export default Gptamin;
+  
